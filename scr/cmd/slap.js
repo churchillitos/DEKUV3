6769dc17c76765e17d20fba2f4cc2960bd12fafe
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-24 19:11:02 +0800

    Update slap.js

--- scr/cmd/slap.js
+++ scr/cmd/slap.js
@@ -13,46 +13,48 @@ module.exports = {
     category: 'fun',
     prefix: false
   },
-  start: async function ({ api, event, args, react, reply }) {
+  start: async function ({ api, text, react, event, reply, User }) {
     let chilli;
 
+    // Determine who to slap
     if (event.type === 'message_reply') {
-        chilli = event.messageReply.senderID;
+      chilli = event.messageReply.senderID;
+    } else if (event.mentions && Object.keys(event.mentions).length > 0) {
+      chilli = Object.keys(event.mentions)[0];
     } else {
-        if (event.mentions && Object.keys(event.mentions).length > 0) {
-            chilli = Object.keys(event.mentions)[0];
-        } else {
-            return reply('Please mention someone to slap, or use the command in reply to a message.');
-        }
+      return reply('Please mention someone to slap, or use the command in reply to a message.');
     }
 
     const pogi = event.senderID;
-    const pangit = (await api.getUserInfo(pogi))[pogi].name;
-    const oubot = (await api.getUserInfo(chilli))[chilli].name;
+    const senderInfo = await api.getUserInfo(pogi);
+    const chilliInfo = await api.getUserInfo(chilli);
+
+    const pangit = senderInfo[pogi].name;
+    const oubot = chilliInfo[chilli].name;
 
     const mapanghi = `${global.deku.ENDPOINT}/canvas/slap?uid=${pogi}&uid2=${chilli}`;
     const filePath = path.resolve(__dirname, 'slap.png');
 
     try {
-        const response = await axios({
-            url: mapanghi,
-            responseType: 'stream',
-        });
+      const response = await axios({
+        url: mapanghi,
+        responseType: 'stream',
+      });
 
-        response.data.pipe(fs.createWriteStream(filePath));
+      response.data.pipe(fs.createWriteStream(filePath));
 
-        response.data.on('end', async () => {
-            await api.sendMessage({
-                body: `${pangit} slapped ${oubot}! 👋`,
-                attachment: fs.createReadStream(filePath),
-            }, event.threadID, () => {
-                fs.unlinkSync(filePath);
-            }, event.messageID);
-        });
+      response.data.on('end', async () => {
+        await api.sendMessage({
+          body: `${pangit} slapped ${oubot}! 👋`,
+          attachment: fs.createReadStream(filePath),
+        }, event.threadID, () => {
+          fs.unlinkSync(filePath);
+        }, event.messageID);
+      });
 
     } catch (error) {
-        console.error('Error:', error);
-        reply('An error occurred while generating the slap image. Please try again later.');
+      console.error('Error:', error);
+      reply('An error occurred while generating the slap image. Please try again later.');
     }
   }
 };
